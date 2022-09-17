647059dd379362daaa667b792163cbb511375f5c
ekosutrisno1 <ekosutrisno801@gmail.com>
2022-09-17 20:40:35 +0700

    update: add the latest chema data for Al-Quran Digital

---
+++ data/ManzilMetaData.ts
@@ -0,0 +1,32 @@
+import { Metadata } from "@/types/alquran.interface";
+
+export const manzilMetadata: Metadata[] = [
+  {
+    surah: 1,
+    ayah: 1
+  },
+  {
+    surah: 5,
+    ayah: 1
+  },
+  {
+    surah: 10,
+    ayah: 1
+  },
+  {
+    surah: 17,
+    ayah: 1
+  },
+  {
+    surah: 26,
+    ayah: 1
+  },
+  {
+    surah: 37,
+    ayah: 1
+  },
+  {
+    surah: 50,
+    ayah: 1
+  }
+]
---
+++ data/index.ts
@@ -0,0 +1,19 @@
+import { suratAndAyat } from './ExoAppJsonSchemaFull';
+import { surahMetadata } from './ExoAppSurahMetaJsonSchema';
+import { juzMetadata } from './JuzMetaData';
+import { rukukMetadata } from './RukukMetaData';
+import { manzilMetadata } from './ManzilMetaData';
+import { sajdaMetadata } from './SajdaMetaData';
+import { pageMetadata } from './PageMetaData';
+import { ayahMetadata } from './ExoAppAyahsJsonSchema';
+
+export {
+    surahMetadata,
+    suratAndAyat,
+    juzMetadata,
+    rukukMetadata,
+    manzilMetadata,
+    sajdaMetadata,
+    pageMetadata,
+    ayahMetadata
+}
